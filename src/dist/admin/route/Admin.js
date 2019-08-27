12d64dda022badfecb2dba81b3fc4b880b5c43e0
Andre <htkandre@gmail.com>
2019-08-26 22:08:00 -0500

    Added Backend

--- src/dist/admin/route/Admin.js
+++ src/dist/admin/route/Admin.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const pug = require('pug');
-const products = require('./products');
-const orders = require('./orders');
 const viewDir = "./admin/view";
 const uuid = require('uuid/v4');
 const hconsole = require('../model/Console');
+
+const inventory = require('./inventory');
+const orders = require('./orders');
+const accounts = require('./accounts');
+
 
 // Validation
 // router.use((req, res, next) =>
@@ -45,19 +48,24 @@ router.route('/login')
   }
 });
 
-router.get(['/', '/home'], (req, res) =>
+router.get(['', '/home'], (req, res) =>
 {
   const orders = [{name: 'Will Smith', id: 347824032, date: '01/20/2003', products: '5 Magic Mangos(5 Packs)', address: '2500 Frosted Green'}];
-  const inventory = [{name: "Magic Mango", price: 19.20, brand: "Teariffic", rating: 4.2, stock: true, id: "34627493289"}];
+  const inventory = [{name: "Magic Mango", price: 19.20, brand: "Teariffic", rating: 4.2, stock: true, id: 34627493289}];
+  const accounts = [{user: "admin", pass: "34242vsdvs", date: "01-20-2003", id: 42342346786482}];
   const page = pug.renderFile(viewDir + '/index.pug', {
     orders,
-    inventory
+    inventory,
+    accounts
   });
   res.send(page);
 });
 
-router.use('/products', products);
+router.use('/inventory', inventory);
 
 router.use('/orders', orders);
+
+router.use('/accounts', accounts);
+
 
 module.exports = router;
--- src/dist/admin/route/orders.js
+++ src/dist/admin/route/orders.js
@@ -2,19 +2,48 @@ const express = require('express');
 const router = express.Router();
 const pug = require('pug');
 const viewDir = "./admin/view";
+const orderDir = "./admin/view/orders";
+const Orders = require("../model/Orders");
 
 router.get('/', (req, res) =>
 {
-  const page = pug.renderFile(viewDir + '/index.pug');
+  const actionsArray = [{name: "Search Order", link: "/admin/orders/search", icon: "fas fa-search"},
+  {name: "Add Order", link: "/admin/orders/add", icon: "fas fa-plus"},
+  {name: "Remove Order", link: "/admin/orders/remove", icon: "fas fa-minus"},
+  {name: "Refund Order", link: "/admin/orders/refund", icon: "fas fa-money-bill"}]
+  const page = pug.renderFile(viewDir + '/layouts/actions.pug',{
+    actions: actionsArray
+  });
   res.send(page);
 });
 
-router.put('/add', (req, res) =>
+router.get('/search', (req, res) =>
 {
-  const pID = req.body.pID;
-  const quan = req.body.quan;
-  const size = req.body.size;
+  const page = pug.renderFile(orderDir + '/search.pug');
+  res.send(page);
+});
 
+router.route('/add')
+.get((req, res) =>
+{
+  if(!req.query.customers)
+  {
+    const page = pug.renderFile(orderDir + '/add.pug');
+    res.send(page);
+  }
+  else
+  {
+    const customers = req.query.customers;
+    const page = pug.renderFile(orderDir + '/add.pug', 
+    {
+      customers
+    });
+    res.send(page);
+  }
+})
+.post((req, res) =>
+{
+  
 });
 
 router.delete('/remove', (req, res) =>
